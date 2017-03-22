42832696ec5bc1a6a83280a7cbac67e8cd0e5cfc
Lewis <Chris.Lewis@nuveen.com>
2017-03-22 10:34:30 -0500

    Inital Commit

---
+++ Get-Colors.ps1
@@ -0,0 +1,18 @@
+﻿$FGcolors = [enum]::GetValues([System.ConsoleColor])
+$BGcolors = [enum]::GetValues([System.ConsoleColor])
+$size = 10
+
+Write-Host "".PadRight($Size).Substring(0,$size) -NoNewline
+Foreach ($BGcolor in $BGcolors) {
+    Write-Host $BGcolor.ToString().PadRight($Size).Substring(0,$size)  -BackgroundColor $BGcolor -NoNewline
+}
+Write-Host 
+Foreach ($FGcolor in $FGcolors) {
+    Write-Host  $FGColor.ToString().PadRight($Size).Substring(0,$size) -foreground $FGColor -NoNewline
+
+    Foreach ($BGcolor in $BGcolors)
+    {
+         Write-Host   $FGColor.ToString().PadRight($Size).Substring(0,$size) -background $BGColor -foreground $FGColor -nonewline
+    }
+    Write-Host 
+}
